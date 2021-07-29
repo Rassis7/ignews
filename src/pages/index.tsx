2e07f4a5dd0824964a9c7864cab76d58b3189821
Romulo Assis <romuloassis.as@gmail.com>
2021-07-29 00:23:20 -0300

    refactor: adicionar lint no arquivo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,12 @@
-import Head from 'next/head'
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>ig.news</title>
-    </Head>
-    <h1>Hello World</h1>
-   </>
-  )
+      <Head>
+        <title>ig.news</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  );
 }
