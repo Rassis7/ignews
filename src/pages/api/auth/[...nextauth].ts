17ed03cc058c062fc56acb31b682d01c81fb1553
Romulo Assis <romuloassis.as@gmail.com>
2021-08-03 11:07:53 -0300

    feat: adicionar querys do fauna

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
-import NextAuth from 'next-auth';
+import { query as q } from 'faunadb';
+import NextAuth, { User } from 'next-auth';
 import Providers from 'next-auth/providers';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   providers: [
@@ -9,4 +11,35 @@ export default NextAuth({
       scope: 'read.user',
     }),
   ],
+  callbacks: {
+    async signIn({ email }: User) {
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email),
+                ),
+              ),
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } },
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email),
+              ),
+            ),
+          ),
+        );
+        return true;
+      } catch (error) {
+        return false;
+      }
+    },
+  },
 });
